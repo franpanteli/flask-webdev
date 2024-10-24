0abc3d985d77a845974213b2f6281705929eb5fd
Francesca Panteli <francescapanteli@Francescas-MacBook-Pro.local>
2024-10-24 13:48:16 +0100

    This commit pushes the results of the hashing task (12.2) in the Flask course.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 #imports
 from . import db
+from werkzeug.security import check_password_hash, generate_password_hash
 
 #defining a model (database) for user roles
 class Role(db.Model):
@@ -18,8 +19,22 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
+    #adding password hashing into the database
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     #to add an age collumn to the database
-    age = db.Column(db.Integer)
+    # age = db.Column(db.Integer)
 
     def __repr__(self):
         return f"<User {self.username}>"
